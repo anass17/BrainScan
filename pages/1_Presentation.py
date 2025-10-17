721d4132e2be568a55b0969902d7d68aa134e6f3
anass17 <anassboutaib2018@gmail.com>
2025-10-17 23:57:22 +0100

    Completed the documentation of the project

--- pages/1_Presentation.py
+++ pages/1_Presentation.py
@@ -1,13 +1,17 @@
-import pandas as pd
-import numpy as np
-import cv2
-import os
-import matplotlib.pyplot as plt
 import streamlit as st
-import tensorflow as tf
-import keras as kr
 
-st.title("les bibliothèques nécessaires:")
+st.markdown("### Présentation générale")
+st.markdown("""
+L'objectif principal du projet est de développer une application intelligente capable de :
+
+Analyser et classer les images IRM du cerveau pour détecter la présence de cancer,
+Assister les médecins dans l'interprétation rapide et fiable des résultats,
+Optimiser le temps de diagnostic tout en réduisant les erreurs humaines.
+
+Ce projet s'inscrit dans une démarche visant à allier santé, innovation et IA afin de renforcer la qualité et la rapidité des soins médicaux au Maroc.
+""")
+
+st.markdown("### Les bibliothèques nécessaires:")
 st.markdown("""
 - **Pandas** : Librairie pour la manipulation et l'analyse de données sous forme de tableaux.
 - **NumPy** : Outil pour le calcul numérique rapide avec des tableaux et des matrices.
